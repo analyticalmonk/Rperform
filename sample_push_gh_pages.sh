949315de477466f60a26ed47ba01d40503eab14b
da-ta-vinci21 <akashtndn.acm@gmail.com>
2015-08-03 16:49:27 +0530

    Added sample file and script for pushing Rperform results from Travis to a repo's gh-pages branch

---
+++ sample_push_gh_pages.sh
@@ -0,0 +1,29 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/"{your_repo_here}"  gh-pages > /dev/null
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/"{your_repo_here}"  master > /dev/null
+
+  #go into master directory and run the Rperform functions
+  cd master
+  touch temp_Rperform.R
+  echo "Rperform::plot_webpage(test_directory = \"./tests/\")" >> temp_Rperform.R
+  Rscript temp_Rperform.R
+  rm temp_Rperform.R
+
+  cd ../gh-pages
+  cp -Rf $HOME/master/index.html index.html
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with Rperform\n"
+fi
