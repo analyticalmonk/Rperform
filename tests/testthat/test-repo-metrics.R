f408bf4fe2a7a05fe4ce6fe7bd7486ecad925c70
analyticalmonk <akashtndn.acm@gmail.com>
2016-08-20 16:18:35 +0530

    Added a test for time_compare()
    - Edited tests/testthat/test-repo-metrics.R

--- tests/testthat/test-repo-metrics.R
+++ tests/testthat/test-repo-metrics.R
@@ -3,7 +3,7 @@ library(testthat)
 
 context("Check if repo metric functionalities work properly")
 
-setwd("./../RperformTestPackage")
+setwd("./RperformTestPackage")
 
 test_that("Wrong parameter type results in error", {
   expect_error(time_compare(test_path = "tests/testthat/test-dup.r", num_commits = "5"))
@@ -17,4 +17,14 @@ test_that("list_commits is working properly", {
                c("msg_list", "date_list", "sha_list"))
 })
 
-setwd("./../testthat")
+temp <- invisible(capture_output(code = Rperform_times <- time_compare(test_path = "./tests/testthat/testTime.R", 
+                                                      num_commits = 4)))
+test_that("time_compare is working properly", {
+  expect_equal(nrow(Rperform_times), 36)
+  expect_equal(ncol(Rperform_times), 7)
+  expect_equal(names(Rperform_times), c("test_name", "metric_name", "status", "metric_val", "message",
+                                        "sha", "date_time"))
+})
+rm(Rperform_times, temp)
+
+setwd("./../")
