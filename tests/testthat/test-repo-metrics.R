4a0b931db9c6fe864c9afe39ec9a56333cf52087
analyticalmonk <akashtndn.acm@gmail.com>
2016-08-20 15:45:50 +0530

    Included dummy package 'RperformTestPackage' and added a test for list_commits
    - Created tests/RperformTestPackage
    - Edited tests/testthat/test-repo-metrics.R

--- tests/testthat/test-repo-metrics.R
+++ tests/testthat/test-repo-metrics.R
@@ -3,20 +3,18 @@ library(testthat)
 
 context("Check if repo metric functionalities work properly")
 
-if(!dir.exists(paths = "./stringr")){
-  git2r::clone(url = "https://github.com/hadley/stringr", local_path = "./stringr")
-  }
-setwd("./stringr")
+setwd("./../RperformTestPackage")
 
 test_that("Wrong parameter type results in error", {
   expect_error(time_compare(test_path = "tests/testthat/test-dup.r", num_commits = "5"))
   expect_error(mem_compare(test_path = "tests/testthat/test-dup.r", num_commits = "5"))
 })
 
-# test_that("Data frames returned by metric functions are of the correct dimensions",{
-#   expect_equal((ncol(time_compare(test_path = "tests/testthat/test-count.r", 2))), 6)
-#   expect_equal(ncol(mem_compare(test_path = "tests/testthat/test-count.r", 2)), 6)
-# 
-# })
-setwd("./../")
-unlink(x = "./stringr", recursive = T, force = T)
+test_that("list_commits is working properly", {
+  expect_equal(nrow(list_commits(path = "./", num_commits = 8)), 8)
+  expect_equal(ncol(list_commits(path = "./", num_commits = 8)), 3)
+  expect_equal(names(list_commits(path = "./", num_commits = 8)),  
+               c("msg_list", "date_list", "sha_list"))
+})
+
+setwd("./../testthat")
