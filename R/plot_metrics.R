66365e3b7dc602f6a1c0edc2b57f162f254d421d
LooDaHu <40339075+LooDaHu@users.noreply.github.com>
2020-04-21 22:38:04 -0700

    Update plot_metrics.R

--- R/plot_metrics.R
+++ R/plot_metrics.R
@@ -830,7 +830,7 @@ run_all_test <- function(num_commits = 1) {
     })
   }
   
- colnames(PeakSegDisk_Result) <- c("num", colnames(PeakSegDisk_Result))
+ colnames(time_data) <- c("num", colnames(time_data))
   .save_data_alt(time_data, pattern = "*.[rR]$", replacement = "_result.csv",
              replace_string = getwd())
 }
